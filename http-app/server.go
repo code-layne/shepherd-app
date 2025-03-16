339bd47c658626d6faf462263e15110516b128d1
Layne Shepherd <layne_s@icloud.com>
2025-03-16 17:44:30 -0400

    using google fonts

--- http-app/server.go
+++ http-app/server.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	router := gin.Default()
-	router.Static("/html", "./html")
-	router.Run(":9000")
+	e := echo.New()
+
+	e.Static("/html", "html")
+	e.GET("/test", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":9000"))
 }
